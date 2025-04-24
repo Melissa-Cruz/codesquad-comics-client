b3a890f6fa78d19a1de323ba4ca7fbdbfa3f96ff
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-04-24 17:29:30 -0400

    working on the handler function for the signup form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,27 +1,65 @@
+import React, {useEffect, useState} from "react"; 
+import { useNavigate } from "react-router-dom";
+
 function Signup({user, setUser}){
+
+    const navigate = useNavigate();
+
+    const handleSignupFormSubmit = (e) =>{
+        e.preventDefault();
+
+        const body = {
+            first_name: e.target.first_name.value,
+            last_name: e.target.last_name.value,
+            email_address: e.target.email_address.value,
+            password: e.target.password.value
+
+
+        }
+
+        const url = "https://course-project-codesquad-comics-server.onrender.com/signup";
+
+        fetch(`${url}`,{
+            method:"POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(body)
+        })
+            .then((response) => response.json())
+            .then((result) => {
+            console.log(result);
+            console.log("success for signup");
+            localStorage.setItem("user", JSON.stringify(result.data));
+            setUser(result.data);
+            navigate(`/admin`);
+        })
+            .catch((error) => console.log(error));
+    };
+
     return (         
         <main>
             <div className="div-white-container">
                 <h1>Sign Up</h1>
                 <div className="div-form-container-center">
-                    <form>
+                    <form onSubmit={handleSignupFormSubmit}>
                         <div>
-                            <label htmlFor="First Name:">First Name:</label>
+                            <label htmlFor="first_name">First Name:</label>
                             <input type="first_name" name="first_name" id="first_name" placeholder="First Name"/>
                         </div>
                         <br/>
                         <div>
-                            <label htmlFor="Last Name:">Last Name:</label>
+                            <label htmlFor="last_name">Last Name:</label>
                             <input type="last_name" name="last_name" id="last_name" placeholder="Last Name"/>
                         </div>
                         <br/>
                         <div>
-                            <label htmlFor="email_address:">Email address:</label>
+                            <label htmlFor="email_address">Email address:</label>
                             <input type="email" name="email_address" id="email_address" placeholder="Email"/>
                         </div>
                         <br/>
                         <div>
-                            <label htmlFor="Password:">Password</label>
+                            <label htmlFor="password">Password</label>
                             <input type="password" name="password" placeholder="password"/>
                         </div>
                         <br/>
